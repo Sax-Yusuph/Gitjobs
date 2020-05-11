17ce97c9a98fc7047a3109f364a5913da63331b1
Sax <yusuphshamsondeen@gmail.com>
2020-05-11 16:32:16 +0100

    refactor ui markup

--- src/components/ResultTemplate.js
+++ src/components/ResultTemplate.js
@@ -1,34 +1,25 @@
-import React, { useState }from 'react';
-import { Box, Stack, Avatar, Text, Flex, Badge, Divider, Button } from '@chakra-ui/core'
+import React from 'react';
+import { Box, Avatar, Text, Flex, Badge, Divider, Button } from '@chakra-ui/core'
 
 const ResultTemplate = () => {
-    const [hover, setHover] = useState(false)
 
-    const hoverTheme = {
-        variant: 'solid',
-        variantColor: 'white',
-        bg: 'purple.500',
-        color: 'white'
-    }
-
-    const handleHover = (e)=> e.nativeEvent.type ==='mouseover' ? setHover(true) : setHover(false)
 
     return ( 
-        <Box bg={hover ? hoverTheme.bg : 'white'} p='5' rounded='lg' shadow='xl' onMouseOver={handleHover} onMouseOut={handleHover}>
+        <Box p='5' rounded='lg' shadow='xl' className='card-link'>
             <Flex>
                 <Avatar src="https://bit.ly/sage-adebayo" />
                 <Box ml="3">
-                    <Text fontWeight="bold" color={hover ? hoverTheme.color : 'blue.800'}>
+                    <Text fontSize='lg' fontWeight="bold" >
                         Senior Product Designer
-                    <Badge float='right' ml='5' variantColor="purple" variant='subtle' rounded='lg' px='2'>
+                    <Badge float='right' ml='5' variantColor="purple" variant='subtle' rounded='lg' px='2' py='1'>
                         Remote
                     </Badge>
                     </Text>
-                    <Text fontSize="sm" color={hover ? hoverTheme.color : 'gray.600'}>Google</Text>
+                    <Text fontSize="sm"  className='text'>Google</Text>
                 </Box>
             </Flex>
             <Divider />
-            <Text color={hover ? hoverTheme.color : 'blue.800'} textAlign='justify' mb='3'>
+            <Text mb='3' fontSize='sm' className='text'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Enim nisi ratione aut magnam commodi et eos, consequuntur 
                 voluptatem, quae iste mollitia dolorem veniam
